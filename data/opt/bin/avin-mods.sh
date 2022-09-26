edaccd93fcb6ca49545021fcbc38f2c81fa7cda4
D3VL Jack <jack@d3vl.com>
2022-09-26 16:35:34 +0100

    commented out all the good stuff

--- data/opt/bin/avin-mods.sh
+++ data/opt/bin/avin-mods.sh
@@ -2,31 +2,22 @@
 
 echo "[avin-mods] start";
 
-# v2 goggle memory locations
+# goggle memory locations
 memloc_map=0x1020f428
 memloc_address=0x1020f42c
 memloc_value=0x1020f430
-
-# TODO: add check if V1 goggles, and set the memory locations accordingly
 
 map_main=0x21
 map_vpp=0x42
 map_csi=0x44
 
-HAS_SET=0;
-
-function setMemToDefaults {
-    echo "[avin-mods] resetting memory values to default";
-    busybox devmem $memloc_map 8 $map_main &&
-    busybox devmem $memloc_address 8 0x0F &&
-    busybox devmem $memloc_value 8 0x00
-}
-
 function writeToRegister {
     address=$1
     value=$2
 
-    busybox devmem $memloc_address 8 $address &&
+    echo "[avin-mods] writing $value to register $address";
+
+    busybox devmem $memloc_address 8 $address && 
     busybox devmem $memloc_value 8 $value
 }
 
@@ -40,32 +31,27 @@ echo "[avin-mods] setting encoding to $ENCODING_INT ($ENCODING_BIN)";
 
 if [ $ENCODING_BIN != "00010100" ]; then
     # disable auto encoding detection
-    writeToRegister 0x07 0x00;
+    # writeToRegister 0x07 0x00; <-- commented out because we can only send one command 
 
     # set the encoding
     writeToRegister 0x02 $ENCODING_INT;
 else 
     # enable auto encoding detection
-    writeToRegister 0x07 0x01;
+    # writeToRegister 0x07 0x01;  <-- commented out because we can only send one command 
 
     # set the encoding to auto
     writeToRegister 0x02 $ENCODING_INT;
 fi
 
+# VV commented out because we can only send one command, so encoding gets it VV
+# Get the values from the config 
+# DISABLE_FREE_RUN=$(package-config getsaved avin-mods disable-free-run)
 
-DISABLE_FREE_RUN=$(package-config getsaved avin-mods disable-free-run)
-
-if [ $DISABLE_FREE_RUN == "true" ]; then    
-    echo "[avin-mods] disabling free run mode";
-    # disable free run mode
-    writeToRegister 0x0C 0x34;
-fi
-
-HAS_SET=1;
-
-echo "[avin-mods] mods applied";
-
-setMemToDefaults;
+# if [ $DISABLE_FREE_RUN == "true" ]; then    
+#     echo "[avin-mods] disabling free run mode";
+#     # disable free run mode
+#     writeToRegister 0x0C 0x34;
+# fi
 
 echo "[avin-mods] completed!";
 
